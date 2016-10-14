08e10607e7034f09ee404c0a42fb1d9e7b82591b
Paul Corbitt <pcorbitt2@gmail.com>
2016-10-14 10:01:59 -0400

    Added comments

--- conversion.py
+++ conversion.py
@@ -1,9 +1,11 @@
 # Conversion Tools
 
 def dollars2cents(dollars):
+	# converts dollars to cents
 	cents = dollars * cents
 	return cents
 
 def gallons2liters(gallons)
+	# convert gallons to liters
 	liters = gallons / 3.78541
 	return liters
