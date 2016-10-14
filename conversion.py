408c0fac8bed896dc8462f2ca906447b8bf81457
Paul Corbitt <pcorbitt2@gmail.com>
2016-10-14 10:13:37 -0400

    Added feet to inches

--- conversion.py
+++ conversion.py
@@ -9,3 +9,10 @@ def gallons2liters(gallons)
 	# convert gallons to liters
 	liters = gallons * 3.78541
 	return liters
+
+def feet2inches(feet)
+	# convert feet to inches
+	inches = feet *12
+	return inches
+
+
